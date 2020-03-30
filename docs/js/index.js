434b49945bbe1e2652336457b3f54adca8056a7a
Osmar Reyes <osmarreyesst@gmail.com>
2020-03-30 14:54:46 -0500

    add markdown translation to html and code higlight

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
-})
+	headerListeners();
+});
+
+
+function headerListeners() {
+	const items = document.querySelectorAll('#content > .main-header .items > *:not(.active)');
+	items.forEach(item => {
+		item.addEventListener('click', () => {
+			const route = item.getAttribute('href');
+			document.location.pathname = route;
+		});
+	});
+}
---
+++ docs/js/markdown.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const box = document.querySelector('.main-content');
+	getMD(box.getAttribute('md-path'), (err, text) => {
+		if (err) throw err;
+		writeMD(text, box);
+		addStyledCodeExamples()
+	})
+})
+/**
+ * Get Markdown text
+ * @param {String} path path to md file
+ * @param {CallableFunction} callback pass 2 arg (error, text)
+ */
+function getMD(path, callback) {
+	fetch(path)
+	.then(resp => {
+		if(resp.status === 200) {
+			resp.text().then(text => {
+				callback(false, text);
+			})
+		}else {
+			callback(new Error('No file found'))
+		}
+	}).catch(err => {
+		callback(err)
+	})
+}
+
+function writeMD(text, box) {
+	let converter = new showdown.Converter();
+	const html = converter.makeHtml(text);
+	box.innerHTML = `<div class="md-content">${html}</div>`;
+}
+
+function addStyledCodeExamples() {
+	hljs.initHighlighting();
+	hljs.configure()
+}
