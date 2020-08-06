24985db43e8969028a20dccbcce1020a1ec125e3
Osmar Reyes <osmarreyesst@gmail.com>
2020-08-05 20:37:59 -0500

    refactor commond functions in metacomponent and metashadowcomponent to metabase

--- __tests__/metaviews.js
+++ __tests__/metaviews.js
@@ -2,6 +2,7 @@ import { MetaComponent } from '../lib/metaviews/meta-component';
 import { MetaShadowComponent } from '../lib/metaviews/meta-shadow-component';
 const Store = require('../lib/store');
 import {Div, DefineElement} from '../lib/customelements/ElementCreator';
+require('../lib/metaviews/meta-base');
 
 let storage = null;
 
@@ -57,9 +58,38 @@ test('MetaComponent with state change', () => {
 			}
 		}
 	}
+
 	DefineElement('stateful-component', myStateFulComponent);
 	document.body.appendChild(StatefulComponent());
 	const el = document.querySelector('stateful-component > div');
+	expect(el.innerHTML).toEqual('1');
+	storage.dispatch({ type: 'INCREMENT' });
+	expect(el.innerHTML).toEqual('2');
+});
+
+test('MetaShadowComponent with state change', () => {
+	storage = new Store({val: 1}, { 'INCREMENT': (a, s) => { s.val++; return {newState: s} } })
+	class myStateFulComponent extends MetaShadowComponent {
+		constructor() {
+			super(storage);
+			this.style = [];
+		}
+		render() {
+			this.content = Div({}, this.storage.getState().val);
+			return this.content;
+		}
+		handleStoreEvents() {
+			return {
+				'INCREMENT': () => {
+					this.content.innerHTML = this.storage.getState().val;
+				}
+			}
+		}
+	}
+
+	DefineElement('statefulshadow-component', myStateFulComponent);
+	document.body.appendChild(StatefulshadowComponent());
+	const el = document.querySelector('statefulshadow-component > div');
 	expect(el.innerHTML).toEqual('1');
 	storage.dispatch({ type: 'INCREMENT' });
 	expect(el.innerHTML).toEqual('2');
