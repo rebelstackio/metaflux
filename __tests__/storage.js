a4cd074ed39ae9651cfce7b4b6542ec446b1ada6
Osmar Reyes <osmarreyesst@gmail.com>
2020-08-18 02:18:14 -0500

    add more coverage to storage

--- __tests__/storage.js
+++ __tests__/storage.js
@@ -17,7 +17,47 @@ test('storage callback after dispatch', () => {
 })
 
 test('storage get handlers', () => {
-	storage = new Store({}, { 'EVENT_TYPE': () => { return { newState: {} } } });
+	storage = new Store({}, { 'EVENT_TYPE': () => { return { newState: {} } },
+	'EVENT_TYPE2': () => { return { newState: {} } } });
 	const _h = storage.getHandlers();
 	expect( typeof _h['EVENT_TYPE'] ).toEqual('function')
+})
+
+test('storage set handler', () => {
+	storage = new Store({}, {});
+	storage.addHandler('EVENT_TYPE', () => { return { newState: {} } } )
+	const _h = storage.getHandlers();
+	expect( typeof _h['EVENT_TYPE'] ).toEqual('function')
+});
+
+test('Deep Cloning', () => {
+	storage = new Store({
+		test: 'x',
+		k: function(x) { return x+1 },
+		j: {
+			z: ['1', '2'],
+			x: /(c)/g,
+			h: {
+				d: new Date(),
+				e: document.createElement('div')
+			}
+		}
+	}, {
+		'UPDATE': (action, state) => {
+			let oldSate = state;
+			state.j.z.push('3');
+			let newState = state;
+			return { newState, oldSate }
+		}
+	});
+	storage.dispatch({type: 'UPDATE'});
+	storage.on('UPDATE', action => {
+		const { newState, oldSate } = action;
+		expect(newState.j.z.lenght).toEqual(3);
+		expect(oldSate.j.z.lenght).toEqual(2);
+	})
+});
+
+test('Empty instance', () => {
+	storage = new Store();
 })
