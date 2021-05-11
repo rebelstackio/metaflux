0c1a702042da6a45b6838a52f5bde72ad5070b4a
Osmar Reyes <osmarreyesst@gmail.com>
2021-05-10 19:30:43 -0500

    add sub menu item, and FAQ section

--- docs/assets/js/index.js
+++ docs/assets/js/index.js
@@ -9,6 +9,7 @@ function setActiveHeaderTab() {
 	const { pathname } = document.location;
 	const tabName = getTabName(pathname);
 	changeHeaderActiveTab(tabName);
+	changeMobileMenu(tabName);
 }
 /**
  * toggle active class to the header elementes
@@ -19,4 +20,30 @@ function changeHeaderActiveTab(name) {
 	const prevActive = document.querySelector('.main-header .active');
 	prevActive.classList.remove('active');
 	newActive.classList.add('active');
+}
+
+function changeMobileMenu(name) {
+	const newActive = document.querySelector(`.mobile-nav *[name="${name}"]`);
+	const prevActive = document.querySelector('.mobile-nav .selected');
+	if (newActive.classList.contains('sub-menu')) {
+		newActive.classList.add('expanded');
+		const content = newActive.querySelector('.sub-menu-content');
+		content.classList.remove('hidden');
+		getSumenuActive(content.children);
+	}
+	prevActive.classList.remove('selected');
+	newActive.classList.add('selected');
+}
+
+function getSumenuActive(childs) {
+	const { pathname } = document.location;
+	for(let i=0; i < childs.length; i++) {
+		let child = childs[i];
+		let name = child.name;
+		name = name.replace(/\//g, '\\/')
+		let isActive = pathname.match(new RegExp(`(${name})`, 'g')) !== null;
+		if (isActive) {
+			child.classList.add('active');
+		}
+	}
 }
